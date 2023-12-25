4664f5191c1c96ef89c49d1cf828bc8b8d625f3f
Piotr Yanushkevich <1999301py@gmail.com>
2023-12-25 19:59:29 +0300

    4-hw fix style

--- src/HW/4/HW4.tsx
+++ src/HW/4/HW4.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import s from './HW4.module.css';
+// import s from './HW4.module.css';
+import '../../App.css';
 import { Button } from './Button';
 import { Input } from './Input';
 
@@ -39,7 +40,7 @@ export const HW4 = () => {
       <ol id={'hw04-tasks'}>
         {texts.map((el, index) => {
           return (
-            <li key={index} id={`hw04-task-${index}`} className={(index % 2) ? s.chetNechet : ''}>
+            <li key={index} id={`hw04-task-${index}`} className={(index % 2) ? 'chetNechet' : ''}>
               {el}
             </li>
           );
